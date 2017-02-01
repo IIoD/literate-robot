c4765d2731ea76756eae92ddd06dad76e3ea7904
degomeda <d.gomez.alvar@gmail.com>
2017-02-01 22:37:23 +0100

    Add files via upload

---
+++ html.php
@@ -0,0 +1,71 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="css/reset.css" rel="stylesheet">
+<link href="css/style.css" rel="stylesheet"> 
+
+<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"
+<![endif]-->
+
+	<title>Instant Database Project</title>
+</head>
+<body>
+<header><h1>Instant Database</h1>
+</header>
+
+<nav class="navigation">
+	
+<div class="select_users">
+	<h2><a href = "#">Logged-in users name</a></h2>
+</div>
+
+<ul class="users">
+<li><a href = "#"> List of the users<a/></li>
+<li><a href = "#"> List of the users<a/></li>
+<li><a href = "#"> List of the users<a/></li>
+</ul>
+
+<ul class="admin">
+<li><a href = "#"> Manage users<a/></li>
+<li><a href = "#"> MAnage movies<a/></li>	
+</ul>
+
+</nav>
+
+<nav class="favs_list">
+	<h2>Favorites</h2>
+<ul class="favs">
+	<li><a href="#">Movie Title</a></li>
+	<li><a href="#">Movie Title</a></li>
+	<li><a href="#">Movie Title</a></li>
+</ul>
+
+</nav>
+
+<section class="movie_list">
+
+	<h2>Hi, username will appear here</h2>
+	<p class="welcome">Here are some movies you might like.
+	Click on the heart icon to add them to your favorite list.</p>
+<ul>
+	<li>
+		<figure><a href ="#"><img class="thumbnail" alt="thumbnail" src="images-movies/thumbnails.png"><a/>
+	<figcaption>
+		<h3><a href="#">Movie title</a></h3>
+	<div class="description">Movie description</div>
+	</figcaption>
+	</figure>
+	</li>
+</ul>
+</section>
+
+
+<footer>
+	<p>Instant update database project by <a href="mailto:d.gomez.alvar@gmail.com"></a></p>
+</footer>
+
+</body>
+</html>
